85bbee9618ad0770ee499ef1c9181f8db0719395
Michael Joyner <joyner.michael@gmail.com>
2019-02-17 12:00:38 +0800

    use more unique port

--- main.go
+++ main.go
@@ -31,5 +31,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3456", nil))
 }
