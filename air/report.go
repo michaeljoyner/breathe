f02f124f26d46962a47a307df3be20d031321bed
Michael Joyner <joyner.michael@gmail.com>
2019-02-17 20:11:22 +0800

    wip

--- air/report.go
+++ air/report.go
@@ -66,6 +66,7 @@ type apiResp struct {
 //GetReport returns the current air quality index report
 func GetReport() (Report, error) {
 	resp, err := http.Get("http://api.waqi.info/feed/taichung/?token=" + os.Getenv("AQI_KEY"))
+	// resp, err := http.Get("http://api.waqi.info/feed/taichung/?token=3da36023636e5377439cf1daffad79cccff9ef3d")
 	if err != nil {
 		return Report{}, err
 	}
