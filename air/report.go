12f0ca79067bb7373d3b77667f92d06c3e490194
Michael Joyner <joyner.michael@gmail.com>
2019-02-17 19:40:11 +0800

    add report message

--- air/report.go
+++ air/report.go
@@ -11,6 +11,42 @@ type Report struct {
 	Station   string
 	Index     int
 	CheckedAt string
+	Message   string
+}
+
+func (r *Report) setMessage() {
+	if r.Index < 0 {
+		r.Message = "Bad input, don't believe anything"
+		return
+	}
+
+	if r.Index < 50 {
+		r.Message = "Super good, breathe it in."
+		return
+	}
+
+	if r.Index < 100 {
+		r.Message = "Acceptable, nothing to worry about."
+		return
+	}
+
+	if r.Index < 150 {
+		r.Message = "Very sensitive people should take caution, fine for most people."
+		return
+	}
+
+	if r.Index < 200 {
+		r.Message = "The air is not good. Limit outdoor exercise."
+		return
+	}
+
+	if r.Index < 300 {
+		r.Message = "Pollution is very bad. Completely avoid outdoor exercise."
+		return
+	}
+
+	r.Message = "Get the hell out of there, we are all doomed"
+
 }
 
 type aqiData struct {
@@ -47,10 +83,14 @@ func GetReport() (Report, error) {
 		return Report{}, err
 	}
 
-	return Report{
+	report := Report{
 		Station:   "Taichung City (Zhongming rd station)",
 		Index:     r.Data.Index,
 		CheckedAt: r.Data.Time.S,
-	}, nil
+	}
+
+	report.setMessage()
+
+	return report, nil
 
 }
