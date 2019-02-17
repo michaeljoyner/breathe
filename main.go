15a9fbfcff8080fd1339c3bd88c8556e463fa8bc
Michael Joyner <joyner.michael@gmail.com>
2019-02-17 12:17:27 +0800

    better error handling

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/alecthomas/template"
 	"github.com/michaeljoyner/breathe/air"
-
-	_ "github.com/joho/godotenv/autoload"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	report, err := air.GetReport()
 	if err != nil {
 		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprint(w, err)
+		return
 	}
 	tpl, err := ioutil.ReadFile("views/base.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
 	}
 	t, err := template.New("webpage").Parse(string(tpl))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
 	}
 
 	t.Execute(w, report)
