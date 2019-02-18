7d38112f0cfbfe04c8977d10c7b05997bec8023e
Michael Joyner <joyner.michael@gmail.com>
2019-02-18 09:08:03 +0800

    little things

--- air/report.go
+++ air/report.go
@@ -12,40 +12,48 @@ type Report struct {
 	Index     int
 	CheckedAt string
 	Warning   string
+	Level     int
 }
 
 func (r *Report) setMessage() {
 	if r.Index < 0 {
 		r.Warning = "Bad input, don't believe anything"
+		r.Level = 0
 		return
 	}
 
 	if r.Index < 50 {
 		r.Warning = "Super good, breathe it in."
+		r.Level = 1
 		return
 	}
 
 	if r.Index < 100 {
 		r.Warning = "Acceptable, nothing to worry about."
+		r.Level = 2
 		return
 	}
 
 	if r.Index < 150 {
 		r.Warning = "Very sensitive people should take caution, fine for most people."
+		r.Level = 3
 		return
 	}
 
 	if r.Index < 200 {
 		r.Warning = "The air is not good. Limit outdoor exercise."
+		r.Level = 4
 		return
 	}
 
 	if r.Index < 300 {
 		r.Warning = "Pollution is very bad. Completely avoid outdoor exercise."
+		r.Level = 5
 		return
 	}
 
 	r.Warning = "Get the hell out of there, we are all doomed"
+	r.Level = 6
 
 }
 
@@ -84,7 +92,7 @@ func GetReport() (Report, error) {
 	}
 
 	report := Report{
-		Station:   "Taichung City (Zhongming rd station)",
+		Station:   "Taichung City",
 		Index:     r.Data.Index,
 		CheckedAt: r.Data.Time.S,
 		Warning:   "Tis is hell",
